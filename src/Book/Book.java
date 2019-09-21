ba0e6ff7734bd995ee8137e963c8f0d49119cbd3
priyarajputcs18 <priya.rajput_cs18@gla.ac.in>
2019-09-21 19:25:53 +0530

    Initial commit

---
+++ src/Book/Book.java
@@ -0,0 +1,42 @@
+package Book;
+
+public class Book {
+    private String name;
+    private String author;
+    private String isbn;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    Book(String name, String author, String isbn) {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "-----------------------------\nBook Name:\t" + getName() + "\nAuthor Name:\t" + getAuthor() + "\nISBN:\t" + getIsbn() + "\n-----------------------------";
+    }
+}
---
+++ src/Book/Solution.java
@@ -0,0 +1,22 @@
+package Book;
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Book[] book = new Book[n];
+        if (n == 0) {
+            System.out.println("N/A");
+        } else {
+            scanner.nextLine();
+            for (int i = 0; i < n; i++) {
+                String name = scanner.nextLine();
+                String author = scanner.nextLine();
+                String isbn = scanner.nextLine();
+                book[i] = new Book(name, author, isbn);
+                System.out.println(book[i]);
+                scanner.close();
+            }
+        }
+    }
+}
